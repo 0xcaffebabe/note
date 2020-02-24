3bf771f99ba6e2801c03a33ff6eff65d4e2f8494
MY <715711877@qq.com>
2020-02-24 14:16:33 +0800

    redis 更新...持久化

--- Statistics.java
+++ Statistics.java
@@ -1,10 +1,19 @@
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.text.DecimalFormat;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.Map.Entry;
 import java.util.stream.Collectors;
+
+import javax.management.RuntimeErrorException;
 
 public class Statistics {
     private static List<File> mdFiles;
@@ -15,6 +24,7 @@ public class Statistics {
         lineCount();
         imgFileCount();
         repositorySize();
+        codeQuantity();
     }
 
     // 统计笔记文件数
@@ -50,30 +60,69 @@ public class Statistics {
     // 统计字数
     private static void wordCount() throws IOException {
         long wordCount = 0;
-        for (var f : getMDFiles()){
+        for (var f : getMDFiles()) {
             var fis = new FileInputStream(f);
             String fileContent = new String(fis.readAllBytes()).trim();
             fileContent = fileContent.replaceAll("\n", "");
-            wordCount+=fileContent.length();
+            wordCount += fileContent.length();
             fis.close();
         }
-        System.out.println("word count:"+wordCount);
+        System.out.println("word count:" + wordCount);
     }
 
     // 统计图片数
     private static void imgFileCount() {
-        System.out.println("image count:"+ listFiles(new File("./assets")).stream().count());
+        System.out.println("image count:" + listFiles(new File("./assets")).stream().count());
     }
 
     // 统计仓库容量
-    private static void repositorySize(){
+    private static void repositorySize() {
         long size = 0;
         List<File> list = listFiles(new File("./"));
-        for(var f : list){
-            size+=f.length();
+        for (var f : list) {
+            size += f.length();
         }
         DecimalFormat df = new DecimalFormat("0.00");
-        System.out.println("repository size:"+df.format(size/1024.0/1024.0)+"MB");
+        System.out.println("repository size:" + df.format(size / 1024.0 / 1024.0) + "MB");
+    }
+
+    // 统计代码占比
+    private static void codeQuantity() {
+        Map<String, Integer> map = new TreeMap<>();
+
+        for (var f : getMDFiles()) {
+            var str = file2String(f);
+            String[] result = str.split("\n");
+            for (var s : result) {
+                if (s.contains("```")) {
+                    s = s.replaceAll("```", "");
+                    s = s.trim();
+
+                    if (s.length() > 0 && s.length() < 10) {
+                        Integer integer = map.get(s);
+                        if (integer == null) {
+                            map.put(s, 1);
+                        } else {
+                            map.put(s, integer + 1);
+                        }
+                    }
+                }
+            }
+
+        }
+        List<Entry<String, Integer>> list = new ArrayList<>(map.entrySet());
+        Collections.sort(list, new Comparator<Map.Entry<String, Integer>>() {
+            @Override
+            public int compare(Entry<String, Integer> o1, Entry<String, Integer> o2) {
+                return o2.getValue().compareTo(o1.getValue());
+            }
+        });
+        System.out.print("code quantity:");
+        for (Entry<String, Integer> e : list) {
+            System.out.print(e.getKey() + ":" + e.getValue() + ", ");
+        }
+        System.out.println();
+
     }
 
     private static List<File> getMDFiles() {
@@ -100,5 +149,16 @@ public class Statistics {
             }
         }
         return ret;
+    }
+
+    private static String file2String(File file) {
+
+        try (FileInputStream fis = new FileInputStream(file)) {
+            String str = new String(fis.readAllBytes());
+            return str;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
     }
 }

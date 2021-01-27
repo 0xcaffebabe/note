252bd595d4b815a6dc8113d37318b28a62670d98
cjiping <cjiping@linewell.com>
2021-01-27 10:05:37 +0800

    🛠更新脚本

--- scripts/build_page_word_count.py
+++ scripts/build_page_word_count.py
@@ -32,7 +32,7 @@ def write_count_to_html(html_file, count):
   if (len(soup.select('.markdown-section .page-footer')) == 0): 
     print(item + "跳过")
     return
-  soup.select('.markdown-section .page-footer')[0].append(BeautifulSoup('<span class="footer-wordcount"> 总字数:%s</span>'%(count),'html.parser'))
+  soup.select('.markdown-section .page-footer')[0].append(BeautifulSoup('<span class="footer-wordcount"> 本页字数:%s</span>'%(count),'html.parser'))
   html = soup.prettify()
   result_file = open(html_file, 'w')
   result_file.write(html)

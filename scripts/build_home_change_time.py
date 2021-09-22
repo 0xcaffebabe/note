21da6c7604beafe84284194c103dfe75b1892839
cjiping <cjiping@linewell.com>
2021-09-22 14:21:12 +0800

    🛠脚本 最后更新时间调整为绝对值

--- scripts/build_home_change_time.py
+++ scripts/build_home_change_time.py
@@ -16,8 +16,9 @@ def process():
         # 只取最近10条
         commit_log = list(commit_log)[0:10]
         days = (datetime.datetime.now() - commit_log[0].author_date.replace(tzinfo=None)).days
+        readable_time = commit_log[0].author_date.strftime("%Y-%m-%d %H:%M:%S")
         i.append(BeautifulSoup("<span class='page-lastupdate' date-time='%s'>%s天前更新</span>" %
-                                   (days, days), features='html.parser'))
+                                   (readable_time, days), features='html.parser'))
         base.log(file_name + " 章节最后更新时间处理完成")
     base.write_text_to_file('_book/index.html', soup.prettify())
 

43d5622fcf75fc504b44f5ce73476360c7621004
cjiping <cjiping@linewell.com>
2021-09-28 19:48:35 +0800

    🛠✨首页更新时间样式优化

--- scripts/js/main.js
+++ scripts/js/main.js
@@ -1,10 +1,24 @@
 console.log("hello world");
 
+function colorMapping(days){
+  if (days <= 10){
+    return 'danger'
+  }else if(days > 10 && days <= 30){
+    return 'warning'
+  }else if(days > 30 && days <= 100){
+    return 'success'
+  }else if(days > 100 && days <= 300){
+    return 'main'
+  }
+  return 'info';
+}
+
 function updatePageLastupdate(){
   const nodeList = document.querySelectorAll(".page-lastupdate");
   for(let item of nodeList) {
     const updateTime = new Date(item.getAttribute("date-time"));
     const days = Math.floor((new Date().getTime() - updateTime.getTime()) / (3600*24*1000));
+    item.classList.add('color-' + colorMapping(days))
     if (days == 0) {
       item.innerHTML = "1天内更新";
     }else {

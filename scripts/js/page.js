a6f8685a46f761ec47c864653d1f44b32b0acef9
cjiping <cjiping@linewell.com>
2021-09-29 16:15:12 +0800

    🛠✨ TOC导航实现

--- scripts/js/page.js
+++ scripts/js/page.js
@@ -1,8 +1,13 @@
 console.log("hello page");
 
 gitbook.events.on('page.change', replaceTocMenu);
+gitbook.events.on('page.change', dirNavigate);
 
-function replaceTocMenu(){
+
+/**
+ * 覆盖toc插件样式
+ */
+function replaceTocMenu() {
   const menu = document.querySelector(".page-toc-menu");
   const button = document.querySelector(".page-toc-button")
   if (menu) {
@@ -11,6 +16,29 @@ function replaceTocMenu(){
   if (button) {
     button.classList.replace('page-toc-button', 'page-toc-button1');
   }
+}
+
+/**
+ * 添加事件以实现目录导航
+ */
+function dirNavigate() {
+  $('.book-body,.body-inner').on('scroll', function () {
+    const idList = document.querySelectorAll('.markdown-section h1, .markdown-section h2, .markdown-section h3, .markdown-section h4 .markdown-section h5, .markdown-section h6');
+    let node = null;
+    for (let i = 0; i < idList.length; i++) {
+      if ($(window).scrollTop() > $(idList[i]).offset().top - 100 || $(this).scrollTop() + $(this).height() == $(document).height()) {
+        node = idList[i];
+      }
+    }
+    if (node != null) {
+      const previousNode = document.querySelector('.page-toc-menu1 .active');
+      if (previousNode != null) {
+        previousNode.classList.remove("active");
+      }
+      console.log(node);
+      document.querySelector(`.page-toc-menu1 a[href='#${node.id}']`).classList.add("active");
+    }
+  });
 }
 
 replaceTocMenu();

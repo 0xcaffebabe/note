2cb9a8c21399cc3d2ca55f91ea48fe32fc23b110
cjiping <cjiping@linewell.com>
2021-10-25 11:12:09 +0800

    🛠删除无用脚本样式

--- doc/scripts/update_links.sh
+++
@@ -1,8 +0,0 @@
-# 处理文章md链接
-if test -z "$(find ./ -type f -name '*.html')";then
-  echo "html文件为空，终止"
-  exit
-fi
-
-find ./ -type f -name '*.html'|xargs sed -i "s/.md#/.html#/g"
-find ./ -type f -name '*.html'|xargs sed -i "s/.md)/.html)/g"

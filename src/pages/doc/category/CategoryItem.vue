bbc6017afccd9078c1c657dd5a4d6e3396a81c70
cjiping <cjiping@linewell.com>
2021-11-19 10:43:38 +0800

    🛠剥离菜单popover内容组件

--- src/pages/doc/category/CategoryItem.vue
+++ src/pages/doc/category/CategoryItem.vue
@@ -6,7 +6,6 @@
     :hide-after="100"
     @show="handlePopoverShow"
     @hide="handlePopoverHide"
-    content="this is content, this is content, this is content"
   >
     <template #reference>
       <el-menu-item
@@ -19,47 +18,20 @@
         </template>
       </el-menu-item>
     </template>
-    <div>
-      <el-skeleton :rows="1" animated :loading="loading" :throttle="500">
-        <template #default>
-          <p>{{ value.name }}</p>
-          <el-badge
-            :value="lastPastDays + '天前更新'"
-            class="item"
-            type="warning"
-          ></el-badge>
-          <el-badge
-            :value="wordCount + '字'"
-            class="item"
-            type="primary"
-          ></el-badge>
-          <div>
-            <el-tag
-              v-for="item in tags"
-              :key="item"
-              size="mini"
-              :type="calcTagType(item)"
-              @click="$router.push('/tag?tag=' + item)"
-              effect="dark"
-              >{{ item }}</el-tag
-            >
-          </div>
-        </template>
-      </el-skeleton>
-    </div>
+    <category-item-content :category-name="value.name" :category-link="value.link" ref="categoryItemContent" />
   </el-popover>
 </template>
 
 <script lang="ts">
 import Category from "@/dto/Category";
 import { defineComponent, PropType } from "vue";
-import api from "@/api";
-import DocFileInfo from "@/dto/DocFileInfo";
-import { cleanText } from "@/util/StringUtils";
 import DocService from "@/service/DocService";
-import TagUtils from "@/pages/tag/TagUtils";
+import CategoryItemContent from "./CategoryItemContent.vue";
 
 export default defineComponent({
+  components: {
+    CategoryItemContent
+  },
   props: {
     value: {
       type: Category as PropType<Category>,
@@ -70,55 +42,22 @@ export default defineComponent({
       deault: false,
     },
   },
-  data() {
-    return {
-      loading: true as boolean,
-      file: null as DocFileInfo | null,
-      // 标签
-      tags: [] as string[],
-      // 离最后一次更新过去几天
-      lastPastDays: 0 as number,
-    };
-  },
-  computed: {
-    wordCount() {
-      return cleanText(this.file?.content).length;
-    },
-  },
   setup() {},
   methods: {
-    calcTagType(tag: string): string {
-      return TagUtils.calcTagType(tag);
-    },
     // 将doc链接转为 x-x-x 形式的id
     convert(link: string): string {
       return DocService.docUrl2Id(link);
     },
     async handlePopoverShow() {
-      this.file = await api.getDocFileInfo(this.convert(this.value!.link));
-      this.tags = DocService.resolveTagList(this.file);
-      this.lastPastDays = this.calcLastUpdate();
-      this.loading = false;
+      (this.$refs.categoryItemContent as any).init();
     },
     handlePopoverHide() {
-      this.loading = true;
+      (this.$refs.categoryItemContent as any).destroy();
     },
     handleMenuItemClick(value: Category) {
       this.$store.commit("setCurrentCategory", value);
     },
-    calcLastUpdate() {
-      if (!this.file) {
-        return -1;
-      }
-      const lastUpdateDate = this.file.commitList
-        .map((v) => v.date)
-        .map(Date.parse)
-        .sort((a, b) => a - b)
-        .reverse()[0];
-      return Math.ceil(
-        (new Date().getTime() - lastUpdateDate) / (3600 * 24 * 1000)
-      );
-    },
+    
   },
 });
 </script>
@@ -127,9 +66,5 @@ export default defineComponent({
 .hilight {
   font-weight: 650;
 }
-.el-tag {
-  cursor: pointer;
-  margin-left: 4px;
-  margin-bottom: 4px;
-}
+
 </style>
---
+++ src/pages/doc/category/CategoryItemContent.vue
@@ -0,0 +1,112 @@
+<template>
+  <div>
+    <el-skeleton :rows="1" animated :loading="loading" :throttle="500">
+      <template #default>
+        <p>{{ categoryName }}</p>
+        <el-badge
+          :value="lastPastDays + '天前更新'"
+          class="item"
+          type="warning"
+        ></el-badge>
+        <el-badge
+          :value="wordCount + '字'"
+          class="item"
+          type="primary"
+        ></el-badge>
+        <div>
+          <el-tag
+            v-for="item in tags"
+            :key="item"
+            size="mini"
+            :type="calcTagType(item)"
+            @click="$router.push('/tag?tag=' + item)"
+            effect="dark"
+            >{{ item }}</el-tag
+          >
+        </div>
+      </template>
+    </el-skeleton>
+  </div>
+</template>
+
+<script lang="ts">
+import Category from "@/dto/Category";
+import DocFileInfo from "@/dto/DocFileInfo";
+import { defineComponent, PropType } from "vue";
+import { cleanText } from "@/util/StringUtils";
+import api from "@/api";
+import DocService from "@/service/DocService";
+import DocUtils from "@/util/DocUtils";
+import TagUtils from "@/pages/tag/TagUtils";
+
+export default defineComponent({
+  props: {
+    categoryName: {
+      type: String,
+      required: true
+    },
+    categoryLink: {
+      type: String,
+      required: true
+    }
+  },
+  computed: {
+    wordCount() {
+      return cleanText(this.file?.content).length;
+    },
+  },
+  methods: {
+    // 初始化内容
+    async init() {
+      this.file = await api.getDocFileInfo(
+        DocUtils.docUrl2Id(this.categoryLink)
+      );
+      this.tags = DocService.resolveTagList(this.file);
+      this.lastPastDays = this.calcLastUpdate();
+      this.loading = false;
+    },
+    // 结束展示
+    destroy() {
+      this.loading = false;
+    },
+    // 计算最后更新于
+    calcLastUpdate() {
+      if (!this.file) {
+        return -1;
+      }
+      const lastUpdateDate = this.file.commitList
+        .map((v) => v.date)
+        .map(Date.parse)
+        .sort((a, b) => a - b)
+        .reverse()[0];
+      return Math.ceil(
+        (new Date().getTime() - lastUpdateDate) / (3600 * 24 * 1000)
+      );
+    },
+    // 计算标签的颜色
+    calcTagType(tag: string): string {
+      return TagUtils.calcTagType(tag);
+    },
+  },
+  data() {
+    return {
+      loading: true as boolean,
+      file: null as DocFileInfo | null,
+      // 标签
+      tags: [] as string[],
+      // 离最后一次更新过去几天
+      lastPastDays: 0 as number,
+    };
+  },
+  setup() {},
+});
+</script>
+
+
+<style lang="less" scoped>
+.el-tag {
+  cursor: pointer;
+  margin-left: 4px;
+  margin-bottom: 4px;
+}
+</style>

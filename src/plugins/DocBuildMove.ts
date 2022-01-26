6c749f9c248b86e6092bf31105a2a14fe34b14de
cjiping <cjiping@linewell.com>
2022-01-26 16:50:38 +0800

    🛠️优化文档内容移动 不使用第三方依赖

--- src/plugins/DocBuildMove.ts
+++ src/plugins/DocBuildMove.ts
@@ -1,10 +1,10 @@
 import { ResolvedConfig } from "vite"
 import fs from 'fs'
 import path from 'path'
-import shell from 'shelljs'
 import rimraf from 'rimraf'
 import BaseService from "../build/BaseService"
 import DocService from '../build/DocService'
+import PathUtils from "../util/PathUtils"
 
 function ensureDirectoryExistence(filePath: string) {
   var dirname = path.dirname(filePath);
@@ -27,9 +27,14 @@ export default function DocBuildMove(){
       if (config.command == 'build') {
         console.log("doc-build-move 清空outDir")
         rimraf.sync(config.build.outDir + "/*")
-        shell.mkdir(config.build.outDir)
+        PathUtils.ensureDirectoryExistence(config.build.outDir)
         console.log("doc-build-move 移动doc目录")
-        shell.cp("-R", "doc/*","./" + config.build.outDir)
+        for(let sourceFile of BaseService.listAllFile('doc')) {
+          const targetFilename = config.build.outDir + '/' + sourceFile.replace('doc/', '')
+          PathUtils.ensureDirectoryExistence(targetFilename)
+          fs.promises.readFile(sourceFile)
+            .then(data => fs.promises.writeFile(targetFilename, data))
+        }
         // 生成md文件对应的json
         for (let item of BaseService.listAllMdFile()) {
           DocService.getFileInfo(item)

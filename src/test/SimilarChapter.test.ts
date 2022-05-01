acf3b4ae6987dd7fbfb6953bb4db8e70a4c528ac
MY <715711877@qq.com>
2022-05-01 23:47:54 +0800

    ⚒️新增文档聚类

---
+++ src/test/SimilarChapter.test.ts
@@ -0,0 +1,126 @@
+import BaseService from '../build/BaseService'
+import fs from 'fs'
+
+var reg = new RegExp("[\\u4E00-\\u9FFF]+", "g");
+
+class ClusterNode {
+  vals: string[] = []
+}
+
+function similar(s: string, t: string, f: number = 3): number {
+  if (!s || !t) {
+      return 0
+  }
+  var l = s.length > t.length ? s.length : t.length
+  var n = s.length
+  var m = t.length
+  var d: number[][] = []
+  f = f || 3
+  var min = function(a: number, b: number, c: number) {
+      return a < b ? (a < c ? a : c) : (b < c ? b : c)
+  }
+  var i, j, si, tj, cost
+  if (n === 0) return m
+  if (m === 0) return n
+  for (i = 0; i <= n; i++) {
+      d[i] = []
+      d[i][0] = i
+  }
+  for (j = 0; j <= m; j++) {
+      d[0][j] = j
+  }
+  for (i = 1; i <= n; i++) {
+      si = s.charAt(i - 1)
+      for (j = 1; j <= m; j++) {
+          tj = t.charAt(j - 1)
+          if (si === tj) {
+              cost = 0
+          } else {
+              cost = 1
+          }
+          d[i][j] = min(d[i - 1][j] + 1, d[i][j - 1] + 1, d[i - 1][j - 1] + cost)
+      }
+  }
+  let res = (1 - d[n][m] / l)
+  return parseFloat(res.toFixed(f))
+}
+
+function cleanText(str: string): string {
+  return str.replace(/[’!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~]/g, '')
+    .replace(/-/g, '')
+    .replace(/#/g, '')
+    .replace(/\s/g, '')
+    .replace(/\|/g, '')
+}
+
+async function  main() {
+  let files = BaseService.listFilesBySuffix("md", "doc").filter(v => v.indexOf("SUMMARY") == -1 && v.indexOf("README") == -1)
+  const map = new Map<string, string>()
+  const similarCache = new Map<string,number>()
+  for(let file of files) {
+    const content = fs.readFileSync(file).toString().split("\n").map(cleanText).map(v => v.trim()).filter(v => v.indexOf("assets") == -1).filter(v => v.length > 20)
+    .filter(v => reg.test(v)).join("\n")
+    map.set(file, content)
+  }
+  // 聚类列表
+  const cluster: ClusterNode[] = []
+  for(let file of files) {
+    const node: ClusterNode = new ClusterNode()
+    node.vals = [file]
+    cluster.push(node)
+  }
+  // 后N轮
+  while(cluster.length >= 10) {
+
+      const cluster1 = cluster.shift()
+      // console.log(cluster1)
+      if (!cluster1) {
+        continue
+      }
+
+      let maxSim = 0
+      let simIndex = -1
+      for(let j = 0; j < cluster.length;j++) {
+        const cluster2 = cluster[j]
+        if (JSON.stringify(cluster1.vals) == JSON.stringify(cluster2.vals)) {
+          continue
+        }
+        let totalSim = 0
+        let cnt = 0
+        for(let file1 of cluster1.vals) {
+          for(let file2 of cluster2.vals) {
+            const key = file1 + "-" + file2
+            const key1 = file2 + "-" + file1
+            if (similarCache.has(key) || similarCache.has(key1)) {
+              totalSim += similarCache.get(key)! || similarCache.get(key1)!
+            }else {
+              const value = similar(map.get(file1)!, map.get(file2)!)
+              similarCache.set(key, value)
+              similarCache.set(key1, value)
+              totalSim += value
+            }
+            cnt++
+          }
+        }
+        if (cnt != 0) {
+          const sim = totalSim / cnt
+          if (sim > maxSim) {
+            maxSim = sim
+            simIndex = j
+          }
+        }
+      }
+  
+      if (simIndex != -1) {
+        const newCluster = new ClusterNode()
+        newCluster.vals = [...cluster1!.vals,...cluster[simIndex].vals]
+        cluster.push(newCluster)
+        cluster.splice(simIndex, 1)
+        console.log(cluster.slice(Math.max(cluster.length - 5, 1)))
+      }
+    }
+  
+    fs.writeFileSync("./docCluster.json", JSON.stringify(cluster))
+  
+}
+main()

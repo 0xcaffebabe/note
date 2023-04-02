d62a33fa13930ae1fe3016a28b61fa28a4f1a825
MY <715711877@qq.com>
2023-04-02 11:50:44 +0800

    ⚒️知识链接流

--- src/service/KnowledgeNetworkService.ts
+++ src/service/KnowledgeNetworkService.ts
@@ -2,7 +2,7 @@
 import api from "@/api";
 import Cache from "@/decorator/Cache";
 import Cacheable from "@/decorator/Cacheable";
-import { KnowledgeLinkNode } from "@/dto/KnowledgeNode";
+import { KnowledgeLinkNode, KnowledgeNode } from "@/dto/KnowledgeNode";
 const cache  = Cache();
 
 class KnowledgeNetworkService implements Cacheable{
@@ -38,6 +38,58 @@ class KnowledgeNetworkService implements Cacheable{
    */
   public getAllLinks(): KnowledgeLinkNode[]{
     return JSON.parse(JSON.stringify(this.links));
+  }
+
+  /**
+   *
+   * 获取某篇文章的知识链接流
+   * @param {string} id
+   * @return {*}  {string[][]}
+   * @memberof KnowledgeNetworkService
+   */
+  public async getDocStream(_id: string): Promise<string[][]> {
+    const network = await api.getKnowledgeNetwork();
+    /*
+      算法概要：
+      1. 针对当前节点的入节点，不断递归，往上找它们的入边
+      2. 针对当前节点的出节点，不断递归，往下找它们的出边
+      递归终止条件：入节点或出节点已经遍历过
+    */
+   
+   const travelSet = new Set<string>()
+   const inputs: string[][] = []
+   const outputs: string[][] = []
+   function getNodeInput(id: string) {
+     if (travelSet.has(id)) {
+      return
+     }
+     travelSet.add(id)
+     const input = network.filter(v => v.links?.some(v => v.id == id)).map(v => v.id)
+     if (input.length != 0) {
+       inputs.unshift(input)
+       input.forEach(getNodeInput)
+     }
+    }
+    function getNodeOutput(id: string) {
+      if (travelSet.has(id)) {
+        return
+      }
+      travelSet.add(id)
+      const temp = network.filter(v => v.id == id)[0]
+      if (!temp) {
+        return
+      }
+      const output = temp.links?.map(v => v.id) || []
+      if (output.length != 0) {
+        outputs.push(output)
+        output.forEach(getNodeOutput)
+      }
+    }
+    getNodeInput(_id)
+    travelSet.clear()
+    getNodeOutput(_id)
+    console.log([...inputs, [_id], ...outputs])
+    return [...inputs, [_id], ...outputs]
   }
 
   /**

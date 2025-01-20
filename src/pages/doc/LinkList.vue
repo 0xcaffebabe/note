e3a4b5e6a59b7c67da8951c72430e6e9e3695515
MY <715711877@qq.com>
2025-01-20 13:49:42 +0800

    🛠修复链接列表失效
    - 修改链接点击事件，移除无效链接
    - 添加鼠标悬停时显示完整链接
    - 调整链接列表样式，增加底部间距和字体大小
    - 优化链接数据处理逻辑，提高效率

--- src/pages/doc/LinkList.vue
+++ src/pages/doc/LinkList.vue
@@ -1,8 +1,8 @@
 <template>
   <el-drawer v-model="showDrawer" size="20%" title="链接列表">
     <ul>
-      <li v-for="item in linkList" :key="item.link">
-        <el-link type="primary" :disabled="!effecientMap[item.link]" @click.prevent="$router.push(item.link);showDrawer = false">
+      <li v-for="item in linkList" :key="item.link" :title="item.link">
+        <el-link type="primary" :disabled="!effecientMap[item.link]" @click.prevent="$router.push(item.link.replace('/#', ''));showDrawer = false">
           {{item.text}}
         </el-link>
       </li>
@@ -11,9 +11,7 @@
 </template>
 
 <script lang="ts">
-import api from '@/api';
 import DocService from '@/service/DocService';
-import DocUtils from '@/util/DocUtils';
 import { defineComponent } from 'vue'
 
 export default defineComponent({
@@ -38,11 +36,10 @@ export default defineComponent({
     show() {
       this.showDrawer = true;
       this.linkList = DocService.resolveLinkList(this.html)
-                                .filter(v => v.link.startsWith('/doc'));
+                                .filter(v => v.link.startsWith('/#/doc'));
       this.linkList.forEach(async v => {
         try {
-          console.log(v.link.replace('/doc/', ''));
-          await DocService.getDocFileInfo(v.link.replace('/doc/', ''));
+          await DocService.getDocFileInfo(v.link.replace('/#/doc/', ''));
           this.effecientMap[v.link] = true;
         }catch(e: any) {
           this.effecientMap[v.link] = false;
@@ -52,3 +49,13 @@ export default defineComponent({
   },
 })
 </script>
+
+<style lang="less" scoped>
+li {
+  padding-left: 40px;
+  padding-bottom: 8px;
+  a {
+    font-size: 16px;
+  }
+}
+</style>

3735baf03111ff347c2e3f9b12b201d979384a89
My <34188127+0xcaffebabe@users.noreply.github.com>
2025-10-09 06:21:42 +0000

    chore(deps): update-browserslist-db

--- components.d.ts
+++ components.d.ts
@@ -18,7 +18,6 @@ declare module 'vue' {
     ElBreadcrumbItem: typeof import('element-plus/es')['ElBreadcrumbItem']
     ElButton: typeof import('element-plus/es')['ElButton']
     ElButtonGroup: typeof import('element-plus/es')['ElButtonGroup']
-    ElCard: typeof import('element-plus/es')['ElCard']
     ElCheckbox: typeof import('element-plus/es')['ElCheckbox']
     ElCol: typeof import('element-plus/es')['ElCol']
     ElContainer: typeof import('element-plus/es')['ElContainer']
